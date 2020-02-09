4adab71abed8156df32b35461e8f134b3fcd09f4
asteine8@gmail.com <psteine3@gmail.com>
2020-02-08 23:24:48 -0800

    fixed rounding errors in guessnum.cpp for asst1

--- assignments/asst1/guessnum.cpp
+++ assignments/asst1/guessnum.cpp
@@ -5,12 +5,26 @@
 *  Alexandra Steiner (oryx26)
 *       => Code modified from guessnum.cpp by T. Harrisburg
 *  
+*
+* # How to win guessnum every time
+*     1: Guess the number halfway between the highest possible number and the lowest possible number
+*     2: If the guess is low, throw out all possibilities below the previous guess
+*     3: If the guess is high, throw out all possibilities above the previous guess
+*     4: Repeat from step 1 until you correctly guess the number
+*     
+* # Instructions for program use
+*     The program guessnum.exe operates exactly like the original program but gives a recommendation for each
+*     guess when asking for a guess. Simply guess the recommended guess each round and you are garunteed to
+*     win.
+*
+*
 *  2/7/2020
 -------------------------------------------------------------------*/
 
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <cmath>
 
 using namespace std;
 
@@ -36,14 +50,14 @@ int intPow(int value, int power) {
 
 int main () {
     srand(time(NULL)) ;              // seed the randomizer
-    int limit = 1001 ;               // set a range limit
-    int secret = rand() % limit ;    // % is the "remainder" operator
+    int limit = 1000 ;               // set a range limit
+    int secret = rand() % (limit+1);    // % is the "remainder" operator
     int max_tries = 10 ;
     int guess;
 
-    int previousGuess = 0;
-    int previousGuessResult = 1;
-    int recommendedGuess;
+    double previousGuess = 0;
+    double previousGuessResult = 1;
+    double recommendedGuess;
 
     int trial;
 
@@ -51,7 +65,7 @@ int main () {
     for (trial = 1; trial <= max_tries; trial++) {
 
         // Calculate the absolute value to move the recommended guess from the previous guess
-        int deltaGuess = limit/(intPow(2,trial));
+        int deltaGuess = ceil((double)limit/((double)intPow(2,trial)));
 
         // Constrain the delta value to a minimum of 1
         deltaGuess = deltaGuess < 1 ? 1 : deltaGuess;
@@ -60,7 +74,7 @@ int main () {
         recommendedGuess = previousGuess + deltaGuess*previousGuessResult;
 
         // Get input from command line and display the recommended guess
-        cout << "Guess (recommended: " << recommendedGuess << "): ";
+        cout << "Guess (recommended: " << (int)round(recommendedGuess) << "): ";
         cin >> guess;
 
         // Compare guess vs secret and display results
@@ -91,6 +105,7 @@ int main () {
     cout << "Number of tries: " << trial << ". Press ENTER:" << endl ;
     cout << "Number was " << secret;
     cin.get() ;
+
     return 0 ;  // (0 is good)
 }
 
--- assignments/asst1/instructions.md
+++ assignments/asst1/instructions.md
@@ -1,8 +1,8 @@
-
 # How to win guessnum every time
     1: Guess the number halfway between the highest possible number and the lowest possible number
     2: If the guess is low, throw out all possibilities below the previous guess
     3: If the guess is high, throw out all possibilities above the previous guess
+    4: Repeat from step 1 until you correctly guess the number
     
 # Instructions for program use
 The program guessnum.exe operates exactly like the original program but gives a recommendation for each
