c3023a4e32b038738d1cbd29a9e8e225099d5875
asteine8@gmail.com <psteine3@gmail.com>
2020-02-18 01:34:15 -0800

    Added search and append method code to chararray.cpp for chararray class project

--- Projects/chararray class/chararray.cpp
+++ Projects/chararray class/chararray.cpp
@@ -60,19 +60,107 @@ void CharArray::toLowercase() {
 }
 
 int CharArray::findTerm(char* searchTerm) {
+    int index = 0;
+    char* tempPtr = array;
+    
+    int numChecks = 0;
 
+    while (*tempPtr != '\0') {
+        while (*searchTerm == *tempPtr) {
+            tempPtr++;
+            searchTerm++;
+            numChecks++;
+
+            if (*searchTerm == '\0') return index;
+            if (*searchTerm != *tempPtr || *tempPtr == '\0') {
+                searchTerm -= numChecks;
+                tempPtr -= numChecks;
+                numChecks = 0;
+                break;
+            }
+        }
+        
+        index++;
+        tempPtr++;
+    }
+
+    return -1;
 }
 
 int CharArray::findTerm(CharArray searchTerm) {
+    int index = 0;
+    char* tempPtr = array;
+    char* searchPtr = searchTerm.array;
+    
+    int numChecks = 0;
 
+    while (*tempPtr != '\0') {
+        while (*searchPtr == *tempPtr) {
+            tempPtr++;
+            searchPtr++;
+            numChecks++;
+
+            if (*searchPtr == '\0') return index;
+            if (*searchPtr != *tempPtr || *tempPtr == '\0') {
+                searchPtr -= numChecks;
+                tempPtr -= numChecks;
+                numChecks = 0;
+                break;
+            }
+        }
+        
+        index++;
+        tempPtr++;
+    }
+
+    return -1;
 }
 
 void CharArray::append(char* str) {
+    int numel = numCharacters(array)+numCharacters(str)-1;
+    char* newArray = new char[numel];
 
+    char* tempPtr = array;
+
+    while (*tempPtr != '\0') {
+        *newArray = *tempPtr;
+        tempPtr++;
+        newArray++;
+    }
+    while (*str != '\0') {
+        *newArray = *str;
+        str++;
+        newArray++;
+    }
+
+    *newArray = '\0';
+    newArray -= numel-1;
+
+    array = newArray;
 }
 
 void CharArray::append(CharArray str) {
+    int numel = numCharacters(array)+numCharacters(str.array)-1;
+    char* newArray = new char[numel];
 
+    char* tempPtr1 = array;
+    char* tempPtr2 = str.array;
+
+    while (*tempPtr1 != '\0') {
+        *newArray = *tempPtr1;
+        tempPtr1++;
+        newArray++;
+    }
+    while (*tempPtr2 != '\0') {
+        *newArray = *tempPtr2;
+        tempPtr2++;
+        newArray++;
+    }
+
+    *newArray = '\0';
+    newArray -= numel-1;
+
+    array = newArray;
 }
 
 // Includes the '\0' char
