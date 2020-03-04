a5fa7737b796351689b1a20c83a42fcdb8c2b5c1
Alexandra Steiner <asteine8@gmail.com>
2020-03-03 18:25:41 -0800

    Added assignment 4

---
+++ assignments/asst4/case.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <iostream>
+
+enum CASE {LOWER, UPPER};
+CASE caseSetting = CASE::LOWER;
+
+// A nice help string to display
+const char* helpinfo = 
+R"(
+    case [ OPTIONS ]
+
+        Reads standard input and changes its case as specified
+        in [ OPTIONS ]
+
+
+    ### Options ###
+
+        -l : Converts all alphabetical characters to lowercase
+
+        -u : Converts all alphabetical characters to uppercase
+
+        -h : Displays this help text
+)";
+
+char toLower(char c);
+char toUpper(char c);
+
+int main(int argc, char** argv) {
+    // Handle Properties
+    for (int i = 1; i < argc; i++) {
+        // Check if this is a property
+        if (argv[i][0] == '-') {
+            switch (argv[i][1]) {
+                case 'l': // To lowercase
+                    caseSetting = CASE::LOWER;
+                    break;
+                case 'u': // To uppercase
+                    caseSetting = CASE::UPPER;
+                    break;
+                case 'h': // Display help
+                    std::puts(helpinfo);
+                    return 0;
+            }
+        }
+    }
+
+    // Basic waterpump
+    register int c = '\0';
+    while (EOF != (c = std::cin.get())) {
+        // Change case
+        switch (caseSetting) {
+            case LOWER:
+                c = toLower(c);
+                break;
+            case UPPER:
+                c = toUpper(c);
+                break;
+        }
+
+        // Write to standard output
+        std::cout.put(c);
+    }
+
+    return 0;
+}
+
+char toLower(char c) {
+    if (c >= 0x41 && c <= 0x5A) {
+        return c + 0x20;
+    }
+}
+char toUpper(char c) {
+    if (c >= 0x61 && c <= 0x7A) {
+        return c - 0x20;
+    }
+}
