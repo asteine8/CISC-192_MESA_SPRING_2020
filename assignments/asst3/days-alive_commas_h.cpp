bb538f9f3f185a419eaa5ce058014909f7acbf87
Alexandra Steiner <asteine8@gmail.com>
2020-05-10 22:48:31 -0700

    Added inclusion of days-alive using profs commas.h

--- assignments/asst3/days-alive_commas_h.cpp
+++ assignments/asst3/days-alive_commas_h.cpp
@@ -1,8 +1,9 @@
-// #include "commas.h"
+#include "commas.h"
 
 #include <stdio.h>
 #include <iostream>
 #include <string>
+
 
 int main() {
     long long years, days, hours, minutes, seconds;
@@ -15,15 +16,25 @@ int main() {
 
 
     std::string yearStr = std::to_string(years);
-    // std::string yearStrWithCommas = commas(yearStr, 3);
+    std::string yearStrWithCommas = commas(yearStr, 3);
 
-    // printf("%-8s: %-25s \n", "Years", yearStrWithCommas);
+    std::string dayStr = std::to_string(days);
+    std::string dayStrWithCommas = commas(dayStr, 3);
 
-    // printf("%-8s: %-20lli\n", "Years", years);
-    // printf("%-8s: %-20lli\n", "Days", days);
-    // printf("%-8s: %-20lli\n", "Hours", hours);
-    // printf("%-8s: %-20lli\n", "Minutes", minutes);
-    // printf("%-8s: %-20lli\n", "Seconds", seconds);
+    std::string hourStr = std::to_string(hours);
+    std::string hourStrWithCommas = commas(hourStr, 3);
+
+    std::string minuteStr = std::to_string(minutes);
+    std::string minuteStrWithCommas = commas(minuteStr, 3);
+
+    std::string secondStr = std::to_string(seconds);
+    std::string secondStrWithCommas = commas(secondStr, 3);
+
+    printf("%-8s: %-25s \n", "Years", yearStrWithCommas.c_str());
+    printf("%-8s: %-25s \n", "Days", dayStrWithCommas.c_str());
+    printf("%-8s: %-25s \n", "Hours", hourStrWithCommas.c_str());
+    printf("%-8s: %-25s \n", "Minutes", minuteStrWithCommas.c_str());
+    printf("%-8s: %-25s \n", "Seconds", secondStrWithCommas.c_str());
 
     return 0;
 }
