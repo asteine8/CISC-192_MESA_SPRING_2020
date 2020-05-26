6af498cb591fa8e279dace4e4bebc42707b30ec4
asteine8@gmail.com <psteine3@gmail.com>
2020-05-26 13:24:12 -0700

    Added some lulu test code for assignment 7

---
+++ assignments/asst7/Lulu Test/test.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdio>
+#include <cctype>
+
+
+using namespace std ;
+
+
+int main (int argc, char *argv[], char **env)
+{
+    char c ;
+    
+    char str;
+    
+    int shift = atoi(argv[1]);
+    do
+    {
+        cin.get() ;
+        
+        if (isalpha(c))
+            {
+                tolower(c);
+                
+                str = c - 97 + shift;
+                
+                c = str % 26;
+                
+                c += 97;
+                
+                cout << c;
+            }
+        else
+        {
+            cout << c;
+        }
+                
+    }
+    
+    while ( ! cin.eof());
+}
